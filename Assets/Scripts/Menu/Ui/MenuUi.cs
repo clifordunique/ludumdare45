714259de9b4fa73122bfa2d2785192f9cdc3dd85
Nathan <nathan.istace@gmail.com>
2019-10-03 01:01:22 +0200

    Ready!

---
+++ Assets/Scripts/Menu/Ui/MenuUi.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class MenuUi : MonoBehaviour {
+	[SerializeField] protected Button _startButton;
+
+	public UnityEvent onStartClicked => _startButton.onClick;
+}

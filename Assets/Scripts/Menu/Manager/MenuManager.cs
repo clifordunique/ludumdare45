a70f3688eacad989780fa355f2b06702d46848b8
Nathan <nathan.istace@gmail.com>
2019-10-03 01:02:07 +0200

    Ready

---
+++ Assets/Scripts/Menu/Manager/MenuManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+	[SerializeField] protected MenuUi _uiController;
+
+	private void Awake() {
+		_uiController.onStartClicked.AddListener(StartGame);
+	}
+
+	private static void StartGame() {
+		SceneManager.LoadSceneAsync("Game");
+	}
+}
